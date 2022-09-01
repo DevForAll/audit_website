8c8eb126f91e87d86bd6ec318d164d479b128db4
kevin cornejo <kevin.cornejo.arango@gmail.com>
2022-09-01 00:58:10 -0500

    Se modifico el texto del menu

--- forms/contact.php
+++ forms/contact.php
@@ -6,14 +6,19 @@
   * For more info and help: https://bootstrapmade.com/php-email-form/
   */
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  // Replace contact@example.com with your real receiving email address 
+  //  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'kevinzito26@hotmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
   } else {
     die( 'Unable to load the "PHP Email Form" Library!');
   }
+
+
+
+
 
   $contact = new PHP_Email_Form;
   $contact->ajax = true;
@@ -24,15 +29,17 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+ 
   /*
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
+    'host' => 'smtp.gmail.com',
+    'username' => 'kevincornejoarango@gmail.com',
+    'password' => '**serelmejortodoslosdias**',
     'port' => '587'
   );
-  */
+*/
 
+  
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
